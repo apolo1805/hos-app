3f3dcd0f43d5426b2ad77ed5a6490cf9a6ea3084
elad <elad@cloudents.com>
2021-05-11 17:42:18 +0300

    Add react-bootstrap + navbar on homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Form, FormControl, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">HOS App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#login">Login</Nav.Link>
+            <Nav.Link href="#signup">Signup</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <HomePage/>
     </div>
   );
